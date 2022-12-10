1a7ee14f56f3476a17c75f212c415b2024e8a4ea
Hedi-hub <72658290+Hedi-hub@users.noreply.github.com>
2022-12-10 18:50:34 +0100

    initial commit

---
+++ src/main/java/com/example/goldblumapp/GoldblumAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.goldblumapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldblumAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoldblumAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/goldblumapp/GoldblumAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goldblumapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldblumAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
